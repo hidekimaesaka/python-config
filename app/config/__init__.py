2931fd042ef2c8673060160d97bd10c7fa614abc
hidekimaesaka <lucasmsk11@gmail.com>
2024-08-31 21:13:53 -0300

    add: unittests

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,4 +1,35 @@
+import os
+
 from dotenv import load_dotenv
 
-def init(mode):
-    ...
+from app.config.base import base_configs
+
+
+def init(mode=None):
+
+    # load .env data on environment variables
+    load_dotenv(override=True)
+    
+    if mode == 'production':
+
+        """
+        if you are using frameworks with dependencie injection pattern
+        like flask, you can receive the app in the params of the init function
+        and inject the base_configs on it
+
+        on our case, we gonna build an script, sou we load the base config on the
+        env vars instead.
+        """
+
+        env = os.environ
+        config = base_configs.get(mode)
+
+        env.update(config)
+
+
+    else:
+
+        env = os.environ
+        config = base_configs.get('development')
+
+        env.update(config)
